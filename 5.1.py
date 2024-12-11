d5953486649a074907e06235762965b9dbd00865
anhpro07 <letuananhbh.2000@gmail.com>
2024-12-11 15:30:06 +0700

    Add files via upload

---
+++ 5.1.py
@@ -0,0 +1,13 @@
+print ("LE TUAN ANH")
+print("235752021610050")
+def square(n):
+ return n*n
+def cube(n):
+ return n*n*n
+def average(values):
+    nvals = len(values)
+    sum = 0.0
+    for v in values:
+        sum += v
+    return float(sum)/nvals
+    
